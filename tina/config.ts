8d59357a2118ae689ba5743f68a2e6f1672cbc20
Yorrick Schoonheydt <cinematenpodcast@gmail.com>
2026-02-04 10:22:40 +0100

    tina

--- tina/config.ts
+++ tina/config.ts
@@ -2,21 +2,12 @@ import { defineConfig } from "tinacms";
 import { NieuwsCollection } from "./collections/nieuws";
 import { ReviewsCollection } from "./collections/reviews";
 
-// Your hosting provider likely exposes this as an environment variable
-const branch =
-  process.env.GITHUB_BRANCH ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
-  process.env.HEAD ||
-  "main";
+const branch = process.env.GITHUB_BRANCH || process.env.VERCEL_GIT_COMMIT_REF || process.env.HEAD || "main";
 
 export default defineConfig({
   branch,
-
-  // Get this from tina.io
   clientId: process.env.PUBLIC_TINA_CLIENT_ID || "68f4665d-7d78-4cfc-90e0-236381fd6e9c",
-  // Get this from tina.io
   token: process.env.TINA_TOKEN || "3f2958f6e4e7461c17694871029a66d6abfbcc34",
-
   build: {
     outputFolder: "admin",
     publicFolder: "public",
